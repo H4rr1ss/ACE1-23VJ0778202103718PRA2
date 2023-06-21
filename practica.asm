97ba9907d059dd1d0501c4b9deaf39de919c7a19
harry <harry7801234@gmail.com>
2023-06-20 21:19:29 -0600

    [add]: Pedida de datos de menú productos

--- practica.asm
+++ practica.asm
@@ -14,13 +14,13 @@ MensajeInicial      db  " Universidad de San Carlos de Guatemala", 0dh, 0ah," Fa
 
 ; SERPARADOR
 separador           db  "----------------------------------------------", 0a, "$"
-separador_sub       db  "============", 0a, "$"
+separador_sub       db  "     ==============", 0a, "$"
 nueva_lin           db  0a, "$" 
 
 ; MENU
-productos           db  "(P)roductos",0a,"$"
+productos           db  "(P)roductoss",0a,"$"
 prompt_codigo       db  "Codigo: ", "$"
-prompt_nombre       db  "Nombre: ", "$"
+prompt_descri       db  "Descripcion: ", "$"
 prompt_precio       db  "Precio: ", "$"
 prompt_unidades     db  "Unidades: ", "$"
 
@@ -32,9 +32,9 @@ prompt              db  "Elija una opcion:",0a,"$"
 temp                db  00, "$"; Cadena temporal
 
 ; IDENTIFICADORES DE CADA MENÚ(encabezado)
-titulo_producto     db  "PRODUCTOS",0a,"$"
-titulo_ventas       db  "VENTAS",0a,"$"
-titulo_herras       db  "HERRAMIENTAS",0a,"$"
+titulo_producto     db  "     | PRODUCTOS  |",0a,"$"
+titulo_ventas       db  "     |   VENTAS   |",0a,"$"
+titulo_herras       db  "     |HERRAMIENTAS|",0a,"$"
 
 ; ETIQUETAS DE INGRESO DE DATOS
 buffer_entrada      db  20, 00
@@ -42,7 +42,7 @@ buffer_entrada      db  20, 00
 
 ; "ESTRUCTURA PRODUCTO"
 cod_prod            db  05 dup (0)
-cod_nomb            db  21 dup (0)
+cod_desc            db  21 dup (0)
 cod_prec            db  03 dup (0)
 cod_unid            db  03 dup (0)
 ;---------------------------------------------------------------------------------------
@@ -98,10 +98,10 @@ menu_productos:
         ; si al es 0 pedirá de nuevo
         cmp al, 00
         je  pedir_de_nuevo_codigo
-        ; si al es menor a 06 acepta la entrada
+        ; si al es menor a 05 acepta la entrada
         cmp al, 05
         jb aceptar_tam_codigo
-        ; si al no era menor a 06, pedira de nuevo 
+        ; si al no era menor a 05, pedira de nuevo 
         jmp pedir_de_nuevo_codigo
         ; MOVER AL CAMPO CODIGO EN LA ESTRUCTURA
     aceptar_tam_codigo:
@@ -111,57 +111,131 @@ menu_productos:
         inc di
         mov cx, [di]
         inc di          ; ME POSICIONO EN EL CONTENIDO DEL BUFFER
-
-    copiar_nombre:  mov al, [di]
+    copiar_nombre_codi:  mov al, [di]
         mov [si], al
         inc si
         inc di
-        loop copiar_nombre  ; RESTARLE 1 A CX, VERIFICAR QUE CX NO SEA 0, SI NO ES 0 VA A LA ETIQUETA
+        loop copiar_nombre_codi  ; RESTARLE 1 A CX, VERIFICAR QUE CX NO SEA 0, SI NO ES 0 VA A LA ETIQUETA
     ; LA CADENA YA FUE INGRESADA EN LA ESTRUCTURA
 
-    ; PEDIR NOMBRE
-        mPrint prompt_nombre
+    ; PEDIR DESCRIPCION
+    pedir_de_nuevo_descri:
+        mPrint prompt_descri
         getData buffer_entrada
         mPrint nueva_lin
+
+        ; VERIFICAR QUE EL TAMAÑO DEL CODIGO NO SEA MAYOR A 32
+        mov di, offset buffer_entrada
+        inc di
+        mov al, [di]
+        ; si al es 0 pedirá de nuevo
+        cmp al, 00
+        je  pedir_de_nuevo_descri
+        ; si al es menor a 33 acepta la entrada
+        cmp al, 21
+        jb aceptar_tam_descri
+        ; si al no era menor a 33, pedira de nuevo 
+        jmp pedir_de_nuevo_descri
+        ; MOVER AL CAMPO CODIGO EN LA ESTRUCTURA
+    aceptar_tam_descri:
+        mov si, offset cod_desc
+        ; OBTENER EL TAMAÑO DE VECES QUE SE MOVERA EL CICLO
+        mov di, offset buffer_entrada
+        inc di
+        mov cx, [di]
+        inc di          ; ME POSICIONO EN EL CONTENIDO DEL BUFFER
+    copiar_nombre_desc:  mov al, [di]
+        mov [si], al
+        inc si
+        inc di
+        loop copiar_nombre_desc  ; RESTARLE 1 A CX, VERIFICAR QUE CX NO SEA 0, SI NO ES 0 VA A LA ETIQUETA
     ; LA CADENA YA FUE INGRESADA EN LA ESTRUCTURA
 
     ; PEDIR PRECIO
+    pedir_de_nuevo_precio:
         mPrint prompt_precio
         getData buffer_entrada
         mPrint nueva_lin
+
+        ; VERIFICAR QUE EL TAMAÑO DEL CODIGO NO SEA MAYOR A 2
+        mov di, offset buffer_entrada
+        inc di
+        mov al, [di]
+        ; si al es 0 pedirá de nuevo
+        cmp al, 00
+        je  pedir_de_nuevo_precio
+        ; si al es menor a 3 acepta la entrada
+        cmp al, 03
+        jb aceptar_tam_precio
+        ; si al no era menor a 3, pedira de nuevo 
+        jmp pedir_de_nuevo_precio
+        ; MOVER AL CAMPO CODIGO EN LA ESTRUCTURA
+    aceptar_tam_precio:
+        mov si, offset cod_prec
+        ; OBTENER EL TAMAÑO DE VECES QUE SE MOVERA EL CICLO
+        mov di, offset buffer_entrada
+        inc di
+        mov cx, [di]
+        inc di          ; ME POSICIONO EN EL CONTENIDO DEL BUFFER
+    copiar_nombre_prec:  mov al, [di]
+        mov [si], al
+        inc si
+        inc di
+        loop copiar_nombre_prec  ; RESTARLE 1 A CX, VERIFICAR QUE CX NO SEA 0, SI NO ES 0 VA A LA ETIQUETA
     ; LA CADENA YA FUE INGRESADA EN LA ESTRUCTURA
 
     ; PEDIR UNIDADES
+    pedir_de_nuevo_unidades:
         mPrint prompt_unidades
         getData buffer_entrada
         mPrint nueva_lin
-    ; LA CADENA YA FUE INGRESADA EN LA ESTRUCTURA
 
-        ;; IMPRIMIR CADENA
-        ;mov bx, 01
-        ;mov di, offset buffer_entrada   ; LA DIRECCION HACIA EL PRIMER BYTE DEL BUFFER
-        ;inc di                          ; SUMARLE 1 A LA DIRECCION DEL PRIMER BYTE, O SEA SE MUEVE A LA DIRECCION DE SIG. BYTE
-        ;; CX = CH + CL, SE COLOCA CH PORQUE PUEDE QUE TENGA UN VALOR
-        ;mov ch, 00
-        ;mov cl, [di]                    ; ACCEDIO A MEMORIA DEL BYTE SIG.
-        ;inc di                          ; SE VUELVE A INCREMENTAR PORQUE LOS DATOS SE ENCUENTRAN A PARTIR DEL 3ER BYTE[ya estamos en 3er byte]
-        ;mov dx, di
-        ;mov ah, 40
-        ;int 21  
+        ; VERIFICAR QUE EL TAMAÑO DEL CODIGO NO SEA MAYOR A 2
+        mov di, offset buffer_entrada
+        inc di
+        mov al, [di]
+        ; si al es 0 pedirá de nuevo
+        cmp al, 00
+        je  pedir_de_nuevo_unidades
+        ; si al es menor a 3 acepta la entrada
+        cmp al, 03
+        jb aceptar_tam_unidades
+        ; si al no era menor a 3, pedira de nuevo 
+        jmp pedir_de_nuevo_unidades
+        ; MOVER AL CAMPO CODIGO EN LA ESTRUCTURA
+    aceptar_tam_unidades:
+        mov si, offset cod_unid
+        ; OBTENER EL TAMAÑO DE VECES QUE SE MOVERA EL CICLO
+        mov di, offset buffer_entrada
+        inc di
+        mov cx, [di]
+        inc di          ; ME POSICIONO EN EL CONTENIDO DEL BUFFER
+    copiar_nombre_unid:  mov al, [di]
+        mov [si], al
+        inc si
+        inc di
+        loop copiar_nombre_unid  ; RESTARLE 1 A CX, VERIFICAR QUE CX NO SEA 0, SI NO ES 0 VA A LA ETIQUETA
+    ; LA CADENA YA FUE INGRESADA EN LA ESTRUCTURA
     jmp fin
+
 ; --------------------------------------------------------------------------
+
 menu_ventas:
     mPrint nueva_lin
     mPrint separador_sub
     mPrint titulo_ventas
     mPrint separador_sub
+    jmp fin
 
 ; --------------------------------------------------------------------------
+
 menu_herramientas:
     mPrint nueva_lin
     mPrint separador_sub
     mPrint titulo_herras
     mPrint separador_sub
+    jmp fin
+
 ; --------------------------------------------------------------------------
 fin:
 
